998f7c9c6b4eb91aa4a408631e31b7b00d1c68e0
artemka-web3 <artemkasidnev.web3@gmail.com>
2023-09-16 18:57:17 +0300

    new v

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { useEffect, useState, useRef } from "react";
-import Web3Modal from "web3modal";
-
-
+import { useEffect, useState } from "react";
 import './assets/css/main.css'
 import main_left_image from "./assets/images/main_left_image.png"
-import detail_icon1 from "./assets/images/detail_icon1.svg"
-import detail_icon2 from "./assets/images/detail_icon2.svg"
-
+import eth_icon from "./assets/images/eth_icon.svg"
+import bottom_arrow from "./assets/images/bottom_arrow.png"
+import pan_icon from "./assets/images/pan_icon.svg"
 import twitter from "./assets/images/twitter.png"
 import discord from "./assets/images/discord.png"
 import medium from "./assets/images/medium.png"
+import detail_1 from "./assets/images/detail_icon1.svg"
+import detail_2 from "./assets/images/detail_icon2.svg"
 
 
-import pantheonContractAbi from './ethereum/jay.json'
+import pantheonContractAbi from './ethereum/pantheon.json'
 import { Contract, providers, utils, Signer } from "ethers";
 
 
-const App = () => {
+function App() {
 
-  let pantheonContractAddress = "0x0106B688463c8ee0F3bc5b15b64Ce33FDE72362c"
-
+  let pantheonContractAddress = "0x16C05396339Ff0e796eb274A2C572dF2e00A8a35"
+  const [provider, setProvider] = useState(null)
+  const [signer, setSigner] = useState(null);
   const [pantheonContract, setPantheonContract] = useState(null)
   const [connected, setConnected] = useState(false);
   const [account, setAccount] = useState("");
@@ -29,12 +29,17 @@ const App = () => {
   const [total_eth_value, setTotalEthValue] = useState("0")
   const [pantheonBalance, setPantheonBalance] = useState("0")
 
+  const [youDepositETH, setYouDepositETH] = useState(0)
+  const [youReceivePantheon, setYouReceivePantheon] = useState(0)
+  const [youBurnPantheon, setYouBurnPantheon] = useState(0)
+  const [youReceiveETH, setYouReceiveETH] = useState(0)
+
 
   const getBalance = async()=>{
     try {
       let balance = await pantheonContract.balanceOf(account);
       balance = utils.formatEther(balance).toString()
-      setPantheonBalance(balance.slice(0, 8) + "...")("en-EN")
+      setPantheonBalance(balance.slice(0, 8) + "...")
 
     } catch (error) {
       console.error('Error:', error);
@@ -45,7 +50,7 @@ const App = () => {
     try {
       let total_eth = await pantheonContract.totalEth();
       total_eth = utils.formatEther(total_eth).toString()
-      setTotalEthValue(total_eth.slice(0, 8) + "...")("en-EN")
+      setTotalEthValue(total_eth.slice(0, 8) + "...")
 
     } catch (error) {
       console.error('Error:', error);
@@ -76,115 +81,278 @@ const App = () => {
     }
   };
 
+	const accountChangedHandler = (newAccount) => {
+		setAccount(newAccount);
+		updateEthers();
+	}
+	const updateEthers = () => {
+		let tempProvider = new providers.Web3Provider(window.ethereum);
+		setProvider(tempProvider);
+
+		let tempSigner = tempProvider.getSigner();
+		setSigner(tempSigner);
+        console.log(tempSigner)
+
+		let tempContract = new Contract(pantheonContractAddress, pantheonContractAbi, tempSigner);
+		setPantheonContract(tempContract);	
+	}
+	const chainChangedHandler = () => {
+		// reload the page to avoid any errors with chain change mid use of application
+		window.location.reload();
+		updateEthers();
+	}
+  window.ethereum.on('accountsChanged', accountChangedHandler);
+
+	window.ethereum.on('chainChanged', chainChangedHandler);
+
   useEffect(()=>{
 
-    connectWallet()
     if (connected){
       setConnBtnText('Connected')
     }
+    connectWallet()
     totalETH()
     getBalance()
   })
-  
+
+
+  const depositHandlerETH = async (event) => {
+    const newValue = event.target.value;
+    setYouDepositETH(newValue);
+    
+    try {
+      const valueInWei = utils.parseEther(newValue); // Convert to Wei
+      const val = await pantheonContract.getMintPantheon(valueInWei);
+      console.log(val);
+      setYouReceivePantheon(utils.formatEther(val));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const burnHandlerPantheon = async (event) => {
+    const newValue = event.target.value;
+    setYouBurnPantheon(newValue);
+    
+    try {
+      const valueInWei = utils.parseEther(newValue); // Convert to Wei
+      const val = await pantheonContract.getRedeemPantheon(valueInWei);
+      console.log(val);
+      setYouReceiveETH(utils.formatEther(val)); // Format as Ether
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+const mint = async () => {
+    try {
+        // First, approve the contract to spend the desired amount of ETH
+        //const amountInETH = youReceivePantheon; // Replace with the amount in ETH you want to deposit
+        //const amountInWei = utils.parseEther(amountInETH.toString());
+        const approvalTx = await pantheonContract.approve(pantheonContractAddress, utils.parseEther(youReceivePantheon));
+        await approvalTx.wait();
+    
+        // Then, call the mint function on the contract
+        const tx = await pantheonContract.mint(account, {
+          value: utils.parseEther(youReceivePantheon),
+        });
+        await tx.wait();
+    
+        console.log("Mint transaction successful");
+      } catch (error) {
+        alert(error);
+      }
+}
+
+const redeemFunc = async () => {
+    try {
+        // Call the redeem function on the contract
+        const amountInETH = youBurnPantheon; // Replace with the amount in ETH you want to deposit
+        const amountInWei = utils.parseEther(amountInETH.toString());
+        const approvalTx = await pantheonContract.approve(pantheonContractAddress, utils.parseEther(youBurnPantheon));
+        await approvalTx.wait();
+        const tx = await pantheonContract.connect(signer).redeem(utils.parseEther(youBurnPantheon));
+        await tx.wait();
+    
+        console.log("Redeem transaction successful");
+      } catch (error) {
+        alert(error);
+      }
+}
 
   return (
+    <div class="wrapper">        
+        <section class="dashboard">
+            <div class="container">
+                <div class="main_block">
+                    <div class="main_left">
+                        <div class="main_left_top">
+                            <a onClick={connectWallet} className="connect_btn">
+                              {connBtnText}
+                            </a>
+                            <div class="dashboard_detail">
+                                <p class="dash_title">Dashboard</p>
+                                <p class="reedem_title">Mint - <br></br> redeem</p>
+                            </div>
+                        </div>
+                        <div class="main_left_img">
+                            <img src={main_left_image} alt=""/>
+                        </div>
+                    </div>
+                    <div class="main_right" id="mint">
+                        <div class="hero_block" id="">
+                            <div class="detail_block">
+                                <div class="detail_item">
+                                    <h3 class="detail_title">
+                                        $pantheon <br></br> <span class="small">owned:</span>
+                                    </h3>
+                                    <div class="item_bottom">
+                                        <div class="item_icon">
+                                            <img src={detail_1} alt=""/>
+                                        </div>
+                                        <p className="item_number">{pantheonBalance}</p>
+                                    </div>
+                                </div>
 
-    <div className="wrapper">
-      <section className="dashboard">
-        <div className="container">
-          <div className="main_block">
-            <div className="main_left">
-              <div className="main_left_top">
-                <a onClick={connectWallet} className="connect_btn">
-                  {connBtnText}
-                </a>
-                <div className="dashboard_detail">
-                  <p className="dash_title active"><a href="/">Dashboard</a></p>
-                  <p className="reedem_title">
-                    <a href="/mint/">Mint - <br /> redeem</a>
-                  </p>
+                                <div class="detail_item">
+                                    <h3 class="detail_title">
+                                        $eth <br></br> <span class="small">Backing amount:</span> 
+                                    </h3>
+                                    <div class="item_bottom">
+                                        <div class="item_icon">
+                                            <img src={detail_2} alt=""/>
+                                        </div>
+                                        <p className="item_number">{total_eth_value}</p>
+                                    </div>
+                                </div>
+
+                                <div class="detail_item">
+                                    <h3 class="detail_title">
+                                        LP APR: <br></br> <span class="small">(Base Swap)</span> 
+                                    </h3>
+                                    <div class="item_bottom">
+                                        <p class="item_number">246.14%</p>
+                                    </div>
+                                </div>
+
+                                <div class="detail_item">
+                                    <h3 class="detail_title">
+                                        TOTAL LP <br></br> <span class="small">Rewards:</span>
+                                    </h3>
+                                    <div class="item_bottom">
+                                        <div class="item_icon">
+                                            <img src={detail_1} alt=""/>
+                                        </div>
+                                        <p class="item_number">12,340.332..</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="money_block">
+                                <div class="money_item">
+                                    <div class="money_top">
+                                        <p class="money_name">MINT</p>
+                                    </div>
+                                    
+                                    <div class="money_wrapper">
+                                        <div class="money_body">
+                                            <div class="money_body_top">
+                                                <p class="top_deposit">you deposit</p>
+                                                <div class="top_money_types top">
+                                                    <div class="money_icon">
+                                                        <img src={eth_icon} alt=""/>
+                                                    </div>
+                                                    <p class="money_icon_name">Eth</p>
+                                                </div>
+                                            </div>
+                                            <input type='text' inputMode='numeric' value={youDepositETH} onChange={depositHandlerETH} className="money_count"/>
+                                        </div>
+                                        <div class="bottom_arrow">
+                                            <img src={bottom_arrow} alt=""/>
+                                        </div>
+                                        <div class="money_body">
+                                            <div class="money_body_top">
+                                                <p class="top_deposit">you recive</p>
+                                                <div class="top_money_types bottom">
+                                                    <div class="money_icon">
+                                                        <img src={pan_icon} alt=""/>
+                                                    </div>
+                                                    <p class="money_icon_name">pantheon</p>
+                                                </div>
+                                            </div>
+                                            <input type='text' inputMode='numeric' value={youReceivePantheon} className="money_count"/>
+                                        </div>
+                                    </div>
+                                    <div className="money_button">
+                                        <a onClick={mint}>Mint</a>
+                                    </div>
+                                </div>
+    
+                                <div class="money_item">
+                                    <div class="money_top">
+                                        <p class="money_name">Redeem</p>
+                                    </div>
+                                    
+                                    <div class="money_wrapper">
+                                        <div class="money_body">
+                                            <div class="money_body_top">
+                                                <p class="top_deposit">you burn</p>
+                                                <div class="top_money_types top">
+                                                    <div class="money_icon">
+                                                        <img src={pan_icon} alt=""/>
+                                                    </div>
+                                                    <p class="money_icon_name">pantheon</p>
+                                                </div>
+                                            </div>
+                                            <input type='text' inputMode='numeric' value={youBurnPantheon} onChange={burnHandlerPantheon} className="money_count"/>
+
+                                        </div>
+                                        <div class="bottom_arrow">
+                                            <img src={bottom_arrow} alt=""/>
+                                        </div>
+                                        <div class="money_body">
+                                            <div class="money_body_top">
+                                                <p class="top_deposit">you receive</p>
+                                                <div class="top_money_types bottom">
+                                                    <div class="money_icon">
+                                                        <img src={eth_icon} alt=""/>
+                                                    </div>
+                                                    <p class="money_icon_name">Eth</p>
+                                                </div>
+                                            </div>
+                                            <input type='text' inputMode='numeric' value={youReceiveETH} className="money_count"/>
+
+                                        </div>
+                                    </div>
+                                    <div className="money_button">
+                                        <a onClick={redeemFunc}>Redeem</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="left_footer">
+                            <div class="footer_text">
+                                <p>docs</p>
+                                <p>media kit</p>
+                            </div>
+                            <p class="footer_name">© Pantheon Ecosystem</p>
+                            <div class="footer_social">
+                                <a href="#" class="social_item">
+                                    <img src={twitter} alt=""/>
+                                </a>
+                                <a href="#" class="social_item">
+                                    <img src={discord} alt=""/>
+                                </a>
+                                <a href="#" class="social_item">
+                                    <img src={medium} alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="main_left_img">
-                <img src={main_left_image} alt="" />
-              </div>
             </div>
-            <div className="main_right">
-              <div className="hero_block">
-                <div className="detail_block">
-                  <div className="detail_item">
-                    <h3 className="detail_title">
-                      $pantheon <br />
-                      <span className="small">owned:</span>
-                    </h3>
-                    <div className="item_bottom">
-                      <div className="item_icon">
-                        <img src={detail_icon1} alt="" />
-                      </div>
-                      <p className="item_number">{pantheonBalance}</p>
-                    </div>
-                  </div>
-                  <div className="detail_item">
-                    <h3 className="detail_title">
-                      $eth <br />
-                      <span className="small">Backing amount:</span>
-                    </h3>
-                    <div className="item_bottom">
-                      <div className="item_icon">
-                        <img src={detail_icon2} alt="" />
-                      </div>
-                      <p className="item_number">{total_eth_value}</p>
-                    </div>
-                  </div>
-                  <div className="detail_item">
-                    <h3 className="detail_title">
-                      LP APR: <br />
-                      <span className="small">(Base Swap)</span>
-                    </h3>
-                    <div className="item_bottom">
-                      <p className="item_number">246.14%</p>
-                    </div>
-                  </div>
-                  <div className="detail_item">
-                    <h3 className="detail_title">
-                      TOTAL LP <br />
-                      <span className="small">Rewards:</span>
-                    </h3>
-                    <div className="item_bottom">
-                      <div className="item_icon">
-                        <img src={detail_icon1} alt="" />
-                      </div>
-                      <p className="item_number">12,340.332..</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="black_block"></div>
-              </div>
-              <div className="left_footer">
-                <div className="footer_text">
-                  <p>docs</p>
-                  <p>media kit</p>
-                </div>
-                <p className="footer_name">© Pantheon Ecosystem</p>
-                <div className="footer_social">
-                  <a href="#" className="social_item">
-                    <img src={twitter} alt="" />
-                  </a>
-                  <a href="#" className="social_item">
-                    <img src={discord} alt="" />
-                  </a>
-                  <a href="#" className="social_item">
-                    <img src={medium} alt="" />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
     </div>
   );
-};
+}
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -3,28 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Mint from './Mint';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/mint/",
-    element: <Mint/>,
-  },
-]);
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
